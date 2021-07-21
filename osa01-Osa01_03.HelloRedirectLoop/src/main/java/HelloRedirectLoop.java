4f7a930e66511f9ceba472e86ddd37720e5b293b
NotMyThingy <miska.janhunen@protonmail.com>
2021-07-21 16:22:14 +0300

    redirecting requests

--- osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
+++ osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
@@ -7,7 +7,35 @@ import java.util.Scanner;
 public class HelloRedirectLoop {
 
     public static void main(String[] args) throws Exception {
+
+        // open server on a port 8080
         ServerSocket server = new ServerSocket(8080);
+
+        while (true) {
+            // listen for requests
+            Socket socket = server.accept();
+
+            // read request
+            Scanner reader = new Scanner(socket.getInputStream());
+
+            if (reader.nextLine().contains("/quit")) {
+                break;
+            }
+
+            // reply
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("HTTP/1.1 302 Found");
+            writer.println("Location: http://localhost:8080");
+            writer.println();
+
+            // close up everything
+            writer.close();
+            reader.close();
+            socket.close();
+
+        }
+
+        server.close();
 
     }
 }
