446215df34fda771f50981cff0a05ed2922d7c92
NotMyThingy <miska.janhunen@protonmail.com>
2021-07-21 12:45:05 +0300

    simple tcp client

--- osa01-Osa01_01.HelloBrowser/src/main/java/HelloBrowser.java
+++ osa01-Osa01_01.HelloBrowser/src/main/java/HelloBrowser.java
@@ -8,5 +8,35 @@ public class HelloBrowser {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("================");
+        System.out.println("THE INTERNETS!");
+        System.out.println("================");
+        System.out.print("Where to? ");
+
+        String whereTo = input.nextLine();
+        int port = 80;
+
+        // create a connection to the server
+        Socket socket = new Socket(whereTo, port);
+
+        // send message to the server
+PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        writer.println("GET / HTTP/1.1");
+        writer.println("Host: " + whereTo);
+        writer.println();
+        writer.flush();
+
+        System.out.println("==========");
+        System.out.println("RESPONSE");
+        System.out.println("==========");
+
+        // read an answer from the server
+        Scanner reader = new Scanner(socket.getInputStream());
+        while (reader.hasNext()) {
+            System.out.println(reader.nextLine());
+        }
+
+        socket.close();
+        input.close();
     }
 }
