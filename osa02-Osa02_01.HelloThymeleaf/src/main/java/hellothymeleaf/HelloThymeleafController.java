a985e08250400a58edf54c418468d28ac749d7e3
NotMyThingy <miska.janhunen@protonmail.com>
2021-07-25 21:11:44 +0300

    Thymeleaf

--- osa02-Osa02_01.HelloThymeleaf/src/main/java/hellothymeleaf/HelloThymeleafController.java
+++ osa02-Osa02_01.HelloThymeleaf/src/main/java/hellothymeleaf/HelloThymeleafController.java
@@ -6,4 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloThymeleafController {
 
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	@GetMapping("/video")
+	public String video() {
+		return "video";
+	}
+
 }
