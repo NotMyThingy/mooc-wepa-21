86799258f1f121d15e0d4b04a2baa147654da463
NotMyThingy <miska.janhunen@protonmail.com>
2021-07-25 21:15:18 +0300

    handling objects with thymeleaf

--- osa02-Osa02_05.HelloObjects/src/main/java/helloobjects/HelloObjectsController.java
+++ osa02-Osa02_05.HelloObjects/src/main/java/helloobjects/HelloObjectsController.java
@@ -18,9 +18,12 @@ public class HelloObjectsController {
         this.items.add(new Item("Wizard hat", "pointy"));
     }
 
-    // Tee tänne juuripolkuun tulevan GET-tyyppisen pyynnön käsittelevä metodi
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("list", items);
+        return "index";
+    }
 
-    // Älä koske tähän metodiin -- tutustumme tiedon lisäämiseen hieman myöhemmin.
     @PostMapping("/")
     public String post(@RequestParam String name, @RequestParam String type) {
         this.items.add(new Item(name.trim(), type.trim()));
