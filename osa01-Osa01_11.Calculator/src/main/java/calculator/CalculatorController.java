2960709eef4369ba9b432b339bdc3ba1249ab697
NotMyThingy <miska.janhunen@protonmail.com>
2021-07-25 21:06:44 +0300

    simple calculator works with path variables

--- osa01-Osa01_11.Calculator/src/main/java/calculator/CalculatorController.java
+++ osa01-Osa01_11.Calculator/src/main/java/calculator/CalculatorController.java
@@ -8,4 +8,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class CalculatorController {
 
+    @GetMapping("/add")
+    @ResponseBody
+    public String add(@RequestParam Integer first, @RequestParam Integer second) {
+        return String.valueOf(first + second);
+    }
+
+    @GetMapping("/subtract")
+    @ResponseBody
+    public String subtract(@RequestParam Integer first, @RequestParam Integer second) {
+        return String.valueOf(first - second);
+    }
+
+    @GetMapping("/multiply")
+    @ResponseBody
+    public String multiply(@RequestParam Integer first, @RequestParam Integer second) {
+        return String.valueOf(first * second);
+    }
+
+    @GetMapping("/devide")
+    @ResponseBody
+    public String devide(@RequestParam Integer first, @RequestParam Integer second) {
+        return String.valueOf(1.0 * first / second);
+    }
 }
